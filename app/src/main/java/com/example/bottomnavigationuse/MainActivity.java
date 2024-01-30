19ed1f627b377699549c90ca28b520f03d464c6d
samet.uyanik <samet.uyanik2756@gmail.com>
2024-01-30 12:56:30 +0300

    BottomNav && NavHostFragment

--- app/src/main/java/com/example/bottomnavigationuse/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationuse/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.bottomnavigationuse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.bottomnavigationuse.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        //NavHostFragment nesnesi oluşturduk yaptığımız tasarım ekranları bu ekranda görünecek.
+        NavHostFragment navHostFragment= (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+
+        //BottomNav ile navHostFragment ikisini birleştime yaptık
+        NavigationUI.setupWithNavController(binding.bottomNav,navHostFragment.getNavController());
+
+        /*
+         * Fragmentlardaki id'ler ile bottom_nav kısmındaki id'leri birbirine eşitledik,
+         * birbirleri ile senkronize çalışabilmesi için bu şekilde gerçekleştirdik.
+         */
+
     }
 }
